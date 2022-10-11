53dba68ed60ce14965bc3f2a9bafb2a1c74c9a55
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-10-11 20:04:09 -0300

    @RafaelCandeias00 [Task: configurando Startup e appsettings] {11/10/2022 20:04}

--- CRUD_Cliente/CRUD_Cliente/Startup.cs
+++ CRUD_Cliente/CRUD_Cliente/Startup.cs
@@ -1,5 +1,9 @@
+using CRUD_Cliente.src.context;
+using CRUD_Cliente.src.repositories;
+using CRUD_Cliente.src.repositories.implements;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,22 +19,38 @@ namespace CRUD_Cliente
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Config Banco de Dados
+            services.AddDbContext<ClientContext>(opt => opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
 
+            // Repositorios
+            services.AddScoped<IClient, ClientRepository>();
+
+            // Controladores
+            services.AddCors();
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ClientContext context)
         {
+            // Ambiente de desenvolviemnto
             if (env.IsDevelopment())
             {
+                context.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
 
+            // Ambiente de produção
+            context.Database.EnsureCreated();
+
+            // Rotas
             app.UseRouting();
+
+            app.UseCors(c => c
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseAuthorization();
 
