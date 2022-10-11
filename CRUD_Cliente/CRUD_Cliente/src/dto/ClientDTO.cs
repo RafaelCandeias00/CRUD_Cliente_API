3bcac83533e98ca1da01b72620e5c13a871e49ce
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-10-11 18:55:39 -0300

    @RafaelCandeias00 [Task: add DTO] {11/10/2022 18:55}

---
+++ CRUD_Cliente/CRUD_Cliente/src/dto/ClientDTO.cs
@@ -0,0 +1,76 @@
+﻿using CRUD_Cliente.src.models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Cliente.src.dto
+{
+    /// <summary>
+    /// <para>Resumo: Classe responsável para transportar um cliente para registro</para>
+    /// <para>Criado por: Rafael Candeias</para>
+    /// </summary>
+    public class NewClient
+    {
+        [Required, StringLength(30)]
+        public string Nome { get; set; }
+
+        [Required, StringLength(11)]
+        public string CPF { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DataNasciemento { get; set; }
+
+        [Required]
+        public Sexo Sexo { get; set; }
+
+        [Required, StringLength(50)]
+        public string Estado { get; set; }
+
+        [Required, StringLength(50)]
+        public string Cidade { get; set; }
+
+        public NewClient(string nome, string cPF, DateTime dataNasciemento, Sexo sexo, string estado, string cidade)
+        {
+            Nome = nome;
+            CPF = cPF;
+            DataNasciemento = dataNasciemento;
+            Sexo = sexo;
+            Estado = estado;
+            Cidade = cidade;
+        }
+    }
+
+    /// <summary>
+    /// <para>Resumo: Classe responsável para transportar um cliente para atualizar</para>
+    /// <para>Criado por: Rafael Candeias</para>
+    /// </summary>
+    public class UpdateClient
+    {
+        [Required, StringLength(30)]
+        public string Nome { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DataNasciemento { get; set; }
+
+        [Required]
+        public Sexo Sexo { get; set; }
+
+        [Required, StringLength(50)]
+        public string Estado { get; set; }
+
+        [Required, StringLength(50)]
+        public string Cidade { get; set; }
+
+        public UpdateClient(string nome, DateTime dataNasciemento, Sexo sexo, string estado, string cidade)
+        {
+            Nome = nome;
+            DataNasciemento = dataNasciemento;
+            Sexo = sexo;
+            Estado = estado;
+            Cidade = cidade;
+        }
+    }
+}
--- CRUD_Cliente/CRUD_Cliente/src/models/ClientModel.cs
+++ CRUD_Cliente/CRUD_Cliente/src/models/ClientModel.cs
@@ -6,14 +6,17 @@ namespace CRUD_Cliente.src.models
 {
     /// <summary>
     /// <para>Resumo: Classe responsável por representar tb_clients no banco</para>
+    /// <para>Criado por: Rafael Candeias</para>
     /// </summary>
     [Table("tb_clients")]
     public class ClientModel
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string CPF { get; set; }
         public string Nome { get; set; }
+        public string CPF { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
         public Sexo Sexo { get; set; }
         public string Estado { get; set; }
